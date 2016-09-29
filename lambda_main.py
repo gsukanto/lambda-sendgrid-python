3fdb1b4d671afdf9c6f469a84dc77f606de3a487
gsukanto <gregory.sukanto@gmail.com>
2016-09-29 11:45:39 +0700

    initial commit (skeleton)

---
+++ lambda_main.py
@@ -0,0 +1,37 @@
+import sendgrid
+import json
+
+SENDGRID_API_KEY = "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"
+EMAIL_FROM = "no-reply@testmokapos.com"
+
+def make_email_to(emails):
+    email_to = []
+    for e in emails:
+        email_to.append({"email": e})
+    return email_to
+
+
+def send_email_handler(event, context):
+    print(event['Records'][0]['Sns']['Message'])
+    template_id = event['Records'][0]['Sns']['Message']['TemplateId']
+    subject = event['Records'][0]['Sns']['Message']['Subject']
+    emails = event['Records'][0]['Sns']['Message']['EmailTo']
+    substitutions = event['Records'][0]['Sns']['Message']['Substitude']
+    data = {
+        "personalizations": [
+            {
+                "to": make_email_to(emails),
+                "subject": subject,
+                "substitutions": substitutions,
+            }
+        ],
+        "from": {
+            "email": EMAIL_FROM
+        },
+        "template_id": template_id
+    }
+    sg = sendgrid.SendGridAPIClient(apikey=SENDGRID_API_KEY)
+    response = sg.client.mail.send.post(request_body=data)
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
